ee9e1df293f2a5248c287fa2eeb78ad21b6dd49b
IcyChlorine <icy_chlorine@pku.edu.cn>
2022-08-11 22:29:19 +0800

    add `Create` alias for `ShowCreation`.

--- custom_animation.py
+++ custom_animation.py
@@ -3,6 +3,8 @@ from manimlib import *
 from manimhub.constants import *
 from manimhub.custom_rate_func import *
 
+# alias
+Create = ShowCreation
 class Popup(GrowFromCenter):
 	CONFIG = {
 		'rate_func': braking_end,
