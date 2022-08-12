2666d546ef4fff9bb02b2df4a873c439f55edf41
IcyChlorine <icy_chlorine@pku.edu.cn>
2022-08-12 15:43:01 +0800

    Add CleanTransform and TransformByIndexMap. The latter is still a prototype and may have bugs. There test modules are also added.

--- custom_animation.py
+++ custom_animation.py
@@ -1,3 +1,5 @@
+from typing import Iterable, Tuple
+
 from manimlib import *
 
 from manimhub.constants import *
@@ -10,3 +12,115 @@ class Popup(GrowFromCenter):
 		'rate_func': braking_end,
 		'clip_alpha': False
 	}
+
+# Transform adds extra points to src. mobj. so it has same #points as tgt. mobj.
+# before interpolating when animation.
+# But these extra points are NOT cleaned up when animation is finished.
+# So I write this CleanTransform tnat cleans up extra points when finished.
+# 
+# This is useful when compliated VMobjects are transformed into simple ones or complex ones,
+# making things clearer and rendering faster.
+#
+# Not sure (not tested) about its compatibility.
+# Suggestion: use it for VMobjects only, when you know its necessary.
+class CleanTransform(Transform):
+	def finish(self) -> None:
+		super().finish()
+		for m1,m2 in zip(self.mobject.get_family(),self.target_mobject.get_family()):
+			l1=len(m1.get_points())
+			l2=len(m2.get_points())
+			if l1>l2:
+				m1.set_points(m2.get_points())
+
+# Only coarsely tested. May have bugs.
+class TransformByIndexMap(AnimationGroup):
+	CONFIG = {
+		"transform_class": CleanTransform
+	}
+	def __init__(self, 
+				mobject: Mobject,
+        		target_mobject: Mobject,
+				keys: Iterable, values: Iterable,
+	 			**kwargs):
+		digest_config(self,kwargs)
+
+		assert(len(keys)==len(values))
+		l1,l2=len(mobject),len(target_mobject)
+		# TODO: assert indexes in keys and values 1)in range of l1&l2, 2)not conflicted 3)ordered
+		assert(True) #suppose they're satisfied here
+		
+		M1=mobject;M2=target_mobject
+		if isinstance(M1,VMobject) and isinstance(M2,VMobject):
+			self.group_class = VGroup 
+		else:
+			self.group_class = Group
+
+		animations = self._compile_animations(mobject,target_mobject,keys,values)
+		super().__init__(*animations, **kwargs)
+
+	def _compile_animations(self, M1, M2, keys, values):
+		animations=[]
+		# convert a bit
+		for i in range(len(keys)):
+			if isinstance(keys[i],int): keys[i]=(keys[i],)
+			if isinstance(values[i],int): values[i]=(values[i],)
+		keys:   Tuple[Tuple[int]]
+		values: Tuple[Tuple[int]]
+
+		# compile		
+		T=self.transform_class
+		if len(keys)==0: return T(M1,M2)
+
+		m1,m2=None,None
+		start_key = range(0,keys[0][0])
+		start_value = range(0,values[0][0])
+		animations.append(self._get_animation(M1,M2,start_key,start_value))
+
+		for i in range(len(keys)):
+			key,value = keys[i],values[i]
+			animations.append(self._get_animation(M1,M2,key,value))
+
+			# 自动补上当中的部分
+			if i==len(keys)-1: continue
+			#not last
+			key_next, value_next = keys[i+1],values[i+1]
+			intermediate_key = range(key[-1]+1,key_next[0])
+			intermediate_value = range(value[-1]+1,value_next[0])
+			animations.append(self._get_animation(M1,M2,intermediate_key,intermediate_value))
+
+		end_key = range(keys[-1][-1]+1,len(M1))
+		end_value = range(values[-1][-1]+1,len(M2))
+		animations.append(self._get_animation(M1,M2,end_key,end_value))
+
+		return animations
+
+	def _get_animation(self, M1,M2,key,value):
+		G=self.group_class
+		T=self.transform_class
+		
+		if len(key)>0: m1=G(*[M1[k] for k in key])
+		else: m1=None
+		if len(value)>0: m2=G(*[M2[v] for v in value])
+		else: m2=None
+
+		if len(key)==0 and len(value)==0: return None
+		elif len(key)==0:   return FadeIn(m2)
+		elif len(value)==0: return FadeOut(m1)
+		else: return T(m1,m2)
+
+	# not used!
+	# a bit subtle. 
+	# 如想debug或修改，建议开个notebook实际运行、试试看。
+	def _parse_index_map(index_map):
+		ans=[]
+		for X,Y in index_map:
+			S=[]
+			while X>10: S.append(X%10);X=X//10
+			S.append(X)
+			X=tuple(S[::-1]) if len(S)>1 else S[0]
+			S=[]
+			while Y>10: S.append(Y%10);Y=Y//10
+			S.append(Y)
+			Y=tuple(S[::-1]) if len(S)>1 else S[0]
+			ans.append((X,Y))
+		return ans
--- test/test_animations.py
+++ test/test_animations.py
@@ -21,3 +21,126 @@ class TestPopup(StarskyScene):
 			self.reset_scene()
 			self.pause()
 
+# Debug 用
+class PointsTracker(VGroup):
+	def __init__(self, mobj: VMobject):
+		super().__init__()
+		# VMobject to track
+		self.mobj=mobj
+		def updater(m: PointsTracker):
+			m.clear()
+			for sub in m.mobj.get_family():
+				for P in sub.data['points']:
+					m.add(Dot(P,radius=0.02,color=YELLOW))
+			return m
+		self.add_updater(updater)
+
+# 由于PointsTracker可视化很慢，因此建议渲染至文件再观察
+class TestCleanTransform(StarskyScene):
+	def pause(self):
+		os.system('pause')
+	def construct(self) -> None:
+		tex1 = self.add(Tex( '\\pi').scale(12))
+		tex2 =          Tex('i\\pi').scale(12)
+		tex1.match_style(Circle())
+		tex2.match_style(Circle())
+
+		points = self.add(PointsTracker(tex1))
+		points_num=Integer(len(points))
+		points_num.add_updater(lambda m: m.set_value(len(points)))
+		self.add(points_num.align_on_border(UL))
+
+		T = CleanTransform
+		self.wait()
+		self.play(T(tex1,tex2,run_time=5))
+		self.wait()
+
+# Just a coarse test now
+# only one case, which is complicated though, is tested.
+# Other cases and edge cases are not tested yet.
+# TODO: A more thorough test
+class TestTransformByIndexMap(StarskyScene):
+	def setup(self):
+		super().setup()
+		fml1=MTex(r'e^{  r }=\lim_{n\to\infty}\left(1+\frac{  r }{n}\right)=-1',isolate=['r',       'e','n'])
+		fml2=MTex(r'e^{i\pi}=\lim_{n\to\infty}\left(1+\frac{i\pi}{n}\right)=-1',isolate=['i','\\pi','e','n'])
+		fml1.set_color_by_tex_to_color_map({'r':RED,               'e':BLUE, 'n':YELLOW})
+		fml2.set_color_by_tex_to_color_map({'i':PURPLE,'\\pi':RED, 'e':BLUE, 'n':YELLOW})
+		self.add(fml1)
+
+		self.fml1,self.fml2=fml1,fml2
+		self.save_state()
+		self.update_frame()
+		
+	def pause(self):
+		os.system('pause')
+	def reset_scene(self):
+		self.restore_state(self.undo_stack[0]);self.update_frame()
+	def construct(self) -> None:
+		#self.test_points_num()
+		#return
+
+		#self.find_index(self.fml2)				
+		#return
+
+		fml1,fml2=self.fml1,self.fml2
+		while True:
+			self.pause()
+
+			# coarse transform
+			#self.play(T(fml1,fml2,run_time=3))
+
+			#对拍用
+			'''T=Transform
+			G=VGroup
+			self.play(
+				T(fml1[0],fml2[0]),
+				T(fml1[1:2],fml2[1:3]),
+				T(fml1[2:12],fml2[3:13]),
+				T(fml1[12:13],fml2[13:15]),
+				T(fml1[13:],fml2[15:]),
+				run_time=3
+			)
+			'''
+
+			T=TransformByIndexMap
+			self.play(T(
+				fml1,fml2,
+				[1,12],
+				[(1,2),(13,14)],
+			))
+
+			self.pause()
+			self.reset_scene()
+		self.embed()
+	def find_index(self,m):
+		self.clear()
+		self.add(m)
+		self.add(index_labels(m))
+		self.update_frame()
+		self.pause()
+
+	def test_points_num(self) -> None:
+		# suppose fml1 ==> e^r =lim_{n→∞}(1+ r/n)^n=-1
+		#         fml2 ==> e^iπ=lim_{n→∞}(1+iπ/n)^n=-1
+		# see below for experiment results.
+		fml1,fml2=self.fml1,self.fml2
+
+		num_points=0
+		for m in self.mobjects:
+			for sub in m.get_family():
+				num_points+=len(sub.get_points())
+		print('before transform, #points = '+str(num_points))
+		# before transform: #points = 2528
+
+		T=CleanTransform
+		self.play(T(fml1,fml2,run_time=1))
+
+		num_points=0
+		for m in self.mobjects:
+			for sub in m.get_family():
+				num_points+=len(sub.get_points())
+		print('after transform, #points = '+str(num_points))
+		# after transform: #points = 
+		# 3083 for Transform
+		# 2885 for CleanTransform
