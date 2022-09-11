00e3f767fdf0dfb74602df8defda628d838930de
IcyChlorine <icy_chlorine@pku.edu.cn>
2022-09-11 15:22:31 +0800

    add ManimStudio, which is an interactive scene that helps dev.

--- my_scene.py
+++ my_scene.py
@@ -10,7 +10,7 @@ else:
 	log.info('OS platform is not windows, some window manipulation based on Win32 API will not work.')
 
 # 注意manimlib在更新boolean_ops之后也有了一个Union，因此import的时候要注意
-from typing import Union,List
+from typing import Union,Optional
 
 # 从package内部import的话要采用这样的语法
 # 参考https://stackoverflow.com/questions/16981921/relative-imports-in-python-3
@@ -281,6 +281,275 @@ class StarskyScene(Scene):
 			self.file_writer.ended_with_interrupt = True
 		self.tear_down()
 
-	#def on_close(self) -> None:
-	#	raise EndScene()
+class SelectionBox(SurroundingRectangle):
+	def surround(self, mobject, **kwargs):
+		buff=MED_SMALL_BUFF
+		self.move_to(mobject)
+		for dim in [0,1]:
+			length = mobject.length_over_dim(dim)
+			self.rescale_to_fit(length+buff,dim,stretch=True)
+		return self
 
+
+class ManimStudio(Scene):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		# meta objects 是用来编辑操作的，例如对对象的标签、选择框等等
+		self.meta_objects=[]
+
+		self.info_labels=[]#info label plays a role of reference list
+		self.bboxes=[] #bounding boxes vis
+		self.points=[] #points vis
+		self.triangulations=[] #traingulation vis
+
+		self.sel_mobject=None
+		self.sel_box=SelectionBox(Dot())
+		self.select(None)
+		self.meta_objects.append(self.sel_box)
+
+		log.info("ManimStudio alpha ver. Keys for selection have been registered, \
+			press ↑↓←→ to select between the hierachy of mobjects.")
+
+	# Select related
+
+	def select(self, mobj: Optional[Mobject], fade_when_desel=True, show_info_label=True):
+		if mobj:
+			assert(isinstance(mobj, Mobject))
+		try: 
+			assert(mobj==None or mobj in self.get_mobject_family_members())
+		except AssertionError:
+			raise ValueError('You must select a mobject in the scene!')
+		self.sel_mobject=mobj
+		if mobj is not None:
+			self.sel_box.surround(mobj)
+			self.sel_box.clear_updaters()
+			self.sel_box.add_updater(lambda m: m.surround(mobj,stretch=True))
+			self.sel_box.set_style(stroke_opacity=1)
+
+			self.clear_info_labels()
+			self.show_info(mobj)
+		else:
+			self.sel_box.surround(Dot(),stretch=True)
+			self.clear_info_labels()
+			if fade_when_desel: self.sel_box.set_style(stroke_opacity=0)
+		return self
+	
+	def _get_proper_parent_in_scene(self,mobj: Mobject) -> Union[Scene,Mobject]:
+		if mobj is None: raise ValueError()
+
+		if mobj in self.mobjects: return self
+		parents=mobj.parents
+		parents=[M for M in parents if M in self.get_mobject_family_members()]
+		return parents[0]
+
+	def select_up(self):
+		if self.mobjects is None: return self
+		if self.sel_mobject is None: 
+			self.select(self.mobjects[0])
+			return self
+
+		parent=self._get_proper_parent_in_scene(self.sel_mobject)
+		if parent is self: return self#到顶了
+		self.select(parent)
+		return self
+	def select_down(self):
+		if self.sel_mobject is None: 
+			return self
+		if len(self.sel_mobject.submobjects)==0:
+			return self
+		for sub in self.sel_mobject:
+			if sub in self.get_mobject_family_members():
+				self.select(sub);break
+		return self
+	def select_previous(self):
+		if self.sel_mobject is None:
+			return self
+
+		parent=self._get_proper_parent_in_scene(self.sel_mobject)
+		if parent is self: 
+			idx=self.mobjects.index(self.sel_mobject)
+			if idx!=0:
+				self.select(self.mobjects[idx-1])
+		else:
+			idx=parent.submobjects.index(self.sel_mobject)
+			if idx!=0:
+				self.select(parent.submobjects[idx-1])
+		return self
+	def select_next(self):
+		if self.sel_mobject is None:
+			return self
+
+		parent=self._get_proper_parent_in_scene(self.sel_mobject)
+		if parent is self: 
+			idx=self.mobjects.index(self.sel_mobject)
+			if idx<len(self.mobjects)-1:
+				self.select(self.mobjects[idx+1])
+		else:
+			idx=parent.submobjects.index(self.sel_mobject)
+			if idx<len(parent.submobjects)-1:
+				self.select(parent.submobjects[idx+1])
+		return self
+
+	def on_key_press(self, symbol: int, modifiers: int) -> None:
+		#super().on_key_press(symbol, modifiers)
+		#print(symbol, modifiers)
+		char = chr(symbol)
+		if char == 'q':
+			self.quit_interaction = True
+		if symbol == ARROW_SYMBOLS[0]: 
+			self.select_previous()
+		if symbol == ARROW_SYMBOLS[1]: 
+			self.select_up()
+		if symbol == ARROW_SYMBOLS[2]: 
+			self.select_next()
+		if symbol == ARROW_SYMBOLS[3]: 
+			self.select_down()
+
+	def show_info(self, mobj: Mobject):
+		if hasattr(mobj,'_info_label_cache'):
+			info_label=mobj._info_label_cache
+		else:
+			#sth like 'manimlib.mobject.number_line.NumberLine'
+			description=str(type(mobj))
+		
+			description=re.match(r".*'(.*)'.*",description).group(1)
+			#simplify the description a bit
+			description= description\
+				.replace('manimlib.mobject.','')\
+				.replace('object.','')
+
+			same_type_mobjs=[m for m in self.get_mobject_family_members() if type(m)==type(mobj)]
+			description+=' #'+str(same_type_mobjs.index(mobj))	
+
+			info_label=Text(description,font_size=25)
+			info_label.next_to(mobj,UP,SMALL_BUFF)
+			info_label.align_to(mobj,LEFT)
+			info_label.shift_onto_screen()
+			def pos_constraint(m: Mobject):
+				m.next_to(mobj,UP,SMALL_BUFF)
+				m.align_to(mobj,LEFT)
+				m.shift_onto_screen()
+
+			info_label.add_updater(pos_constraint)
+
+		self.meta_objects.append(info_label)
+		self.info_labels.append(info_label)
+		mobj._info_label_cache=info_label
+	def clear_info_labels(self):
+		for label in self.info_labels:
+			self.meta_objects.remove(label)
+			del label
+		self.info_labels=[]
+
+	def show_bounding_box(self, mobj: Mobject):
+		bbox_arr=mobj.data['bounding_box']
+		corners_dl=bbox_arr[0]
+		corners_ur=bbox_arr[2]
+		center=bbox_arr[1]
+		X_MASK=np.array([1.,0.,0.])
+		Y_MASK=np.array([0.,1.,0.])
+		corners_dr=corners_dl*Y_MASK+corners_ur*X_MASK
+		corners_ul=corners_dl*X_MASK+corners_ur*Y_MASK
+
+		bbox_vis=Polygon(corners_dl,corners_dr,corners_ur,corners_ul,color='#777777')
+		bbox_vis.add(Dot(center,color='#AAAAAA'))
+
+		self.bboxes.append(bbox_vis)
+		self.meta_objects.append(bbox_vis)
+
+	show_bbox=show_bounding_box # function alias
+
+	def clear_boundingx_boxes(self):
+		for bbox in self.bboxes:
+			self.meta_objects.remove(bbox)
+			del bbox
+		self.bboxes=[]
+
+	clear_bboxes=clear_boundingx_boxes
+
+	# Show points (bazier anchor and handles actually) of VMobject
+
+	def show_points(self, mobj: VMobject, show_index=False, **dot_kwargs):
+		assert(isinstance(mobj,VMobject))
+		
+		dot_kwargs.setdefault('radius', 0.03)
+		dot_kwargs.setdefault('color' , WHITE)
+
+		points_vis=VGroup(*[Dot(d,**dot_kwargs) for d in mobj.data['points']])
+
+		if show_index:
+			for i,d in enumerate(mobj.data['points']):
+				label=Integer(i,color=RED).set_height(0.2)\
+					.next_to(points_vis[i],UP,buff=0.02)
+				if i%3==0:
+					label.next_to(points_vis[i],RIGHT,buff=0.02)
+				points_vis[i].add(label)
+		self.points.append(points_vis)
+		self.meta_objects.append(points_vis)
+
+		log.info(f"{len(mobj.data['points'])} points showed in total.")
+
+	def clear_points(self):
+		for g in self.points:
+			self.meta_objects.remove(g)
+			del g
+		self.points=[]
+
+	# Show triangulation for VMobject (still in experiment, not done)
+	def show_triangulation(self, mobj: VMobject):
+		log.warning("show_triangulation(..) is still in dev and may have unexpected results. Be aware.")
+		assert(isinstance(mobj,	VMobject))
+
+		tri_vis=VGroup()
+		points=mobj.data['points']
+		tri=mobj.triangulation
+		ntri=len(mobj.triangulation)//3
+		MyTriangle=Polygon
+		for t in range(ntri):
+			tri_vis.add(MyTriangle(
+				points[tri[3*t]],
+				points[tri[3*t+1]],
+				points[tri[3*t+2]],
+				stroke_width=2
+			))
+		self.triangulations.append(tri_vis)
+		self.meta_objects.append(tri_vis)
+
+	def clear_triangulations(self):
+		for tri in self.triangulations:
+			self.meta_objects.remove(tri)
+			del tri
+		self.triangulations=[]
+
+	# Override update_frame so that meta_objects can be rendered.
+	# remenber that meta_objects are mobjects that shows the info
+	# of mobjects, hence they belong to "studio", not the "scene".
+	#
+	# Not checking _should_update here, so it doesn't support 
+	# hot reload yet.
+	def update_frame(self, dt=0, ignore_skipping=False):
+		self.increment_time(dt)
+		self.update_mobjects(dt)
+		if self.skip_animations and not ignore_skipping:
+			return
+
+		if self.window:
+			self.window.clear()
+		self.camera.clear()
+		self.camera.capture(*self.mobjects, *self.meta_objects)
+
+		if self.window:
+			self.window.swap_buffers()
+			vt = self.time - self.virtual_animation_start_time
+			rt = time.time() - self.real_animation_start_time
+			if rt < vt:
+				self.update_frame(0)
+
+	# Override update_mobjects(...) to update meta_objects properly
+	def update_mobjects(self, dt):
+		for mobject in self.mobjects:
+			mobject.update(dt)
+		for object in self.meta_objects:
+			object.update(dt)
+
+	
