f214702a0121a64af0d6a8c24f9458078230aa4e
IcyChlorine <icy_chlorine@pku.edu.cn>
2023-02-05 10:55:09 +0800

    add Curtain

--- custom_mobjects.py
+++ custom_mobjects.py
@@ -11,3 +11,59 @@ class Cross(SVGMobject):
 	def __init__(self, **kwargs):
 		super().__init__('assets/cross.svg', **kwargs)
 		self.set_stroke(width=0)
+
+# 用于遮挡后面mobj的幕布
+# 用的时候请注意mobj图层顺序——Curtain类本身不保证遮挡关系。
+class Curtain(Rectangle):
+	CONFIG = {
+		'fill_opacity': 1,
+		'fill_color': BG_COLOR,
+		'stroke_width': 0,
+		'stroke_opacity': 0,
+
+		# effective only 1)when init 2)when lrdu options are not specified
+		'scale_factor': 1.0, 
+		'left': -FRAME_WIDTH/2,
+		'right':+FRAME_WIDTH/2,
+		'up':   +FRAME_HEIGHT/2,
+		'down': -FRAME_HEIGHT/2
+	}
+	def __init__(self, **kwargs):
+		digest_config(self, kwargs)
+		self.left *= self.scale_factor
+		self.right*= self.scale_factor
+		self.up   *= self.scale_factor
+		self.down *= self.scale_factor
+
+		super().__init__(self.right-self.left, self.up-self.down)
+
+		self._put_into_place()
+
+	def _put_into_place(self):
+		self.next_to(RIGHT*self.left, RIGHT, buff=0, coor_mask = np.array([1,0,0]))
+		self.next_to(  UP *self.down,   UP , buff=0, coor_mask = np.array([0,1,0]))
+
+	def set_left(self, left):
+		if isinstance(left, np.ndarray): left=left[0]
+		self.left=left
+		self.rescale_to_fit(self.right-self.left, dim=0, stretch=True)
+		self._put_into_place()
+		return self
+	def set_right(self, right):
+		if isinstance(right, np.ndarray): right=right[0]
+		self.right=right
+		self.rescale_to_fit(self.right-self.left, dim=0, stretch=True)
+		self._put_into_place()
+		return self
+	def set_up(self, up):
+		if isinstance(up, np.ndarray): up=up[1]
+		self.up=up
+		self.rescale_to_fit(self.up-self.down, dim=1, stretch=True)
+		self._put_into_place()
+		return self
+	def set_down(self, down):
+		if isinstance(down, np.ndarray): down=down[1]
+		self.down=down
+		self.rescale_to_fit(self.up-self.down, dim=1, stretch=True)
+		self._put_into_place()
+		return self
