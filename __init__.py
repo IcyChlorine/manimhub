035c2db05a6e7d0d6b321ebcf997b56a5d0f8b67
IcyChlorine <icy_chlorine@pku.edu.cn>
2023-07-03 15:49:00 +0800

    release DataAxes from manimhub

--- __init__.py
+++ __init__.py
@@ -5,4 +5,5 @@ from manimhub.my_scene import *
 from manimhub.custom_rate_func import *
 from manimhub.custom_animation import *
 from manimhub.custom_mobjects import *
+from manimhub.data_axes import *
 from manimhub.little_creature import *
